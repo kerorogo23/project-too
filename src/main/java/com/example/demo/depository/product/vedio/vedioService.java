de13e29bd33426c0e76a8aeaaa09bbc736b6cd30
kerorogo23 <saddjk666@gmail.com>
2024-01-22 22:00:02 +0800

    update

--- src/main/java/com/example/demo/depository/product/vedio/vedioService.java
+++ src/main/java/com/example/demo/depository/product/vedio/vedioService.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class vedioService {
-    //test
-    //test2
-    //test3
-    //test4
-    //test5
-    //test6
+   
+    public void getAll(){
+        
+    }
 }
